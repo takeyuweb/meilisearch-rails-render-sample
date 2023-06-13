73a356b49945b6acafe19c5cbccc522bd162138c
Yuichi Takeuchi <yuichi.takeuchi@takeyuweb.co.jp>
2023-06-13 19:54:45 +0000

    add pagination

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -1,14 +1,17 @@
 class ZipcodesController < ApplicationController
+  include Pagy::Backend
+
   def index
     @query = params[:query]
     if @query.present?
       expression = "%#{ApplicationRecord.sanitize_sql_like(@query)}%"
-      @zipcodes = Zipcode.where("code LIKE ?", expression)
+      zipcodes = Zipcode.where("code LIKE ?", expression)
         .or(Zipcode.where("prefecture || city || neighborhood LIKE ?", expression))
         .or(Zipcode.where("prefecture_kana || city_kana || neighborhood_kana LIKE ?", expression))
-        .limit(30)
     else
-      @zipcodes = Zipcode.none
+      zipcodes = Zipcode.none
     end
+
+    @pagy, @zipcodes = pagy(zipcodes)
   end
 end
--- app/helpers/zipcodes_helper.rb
+++ app/helpers/zipcodes_helper.rb
@@ -1,2 +1,3 @@
 module ZipcodesHelper
+  include Pagy::Frontend
 end
